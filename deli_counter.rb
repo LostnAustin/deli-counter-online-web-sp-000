7272a9af615a6b1601a01f9c27467f00aff3cbdd
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-26 21:11:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,13 +19,9 @@ def line(positions)
     end
   end
 
-
-
   #Build a method that a new customer will use when entering the deli.
-  #The take_a_number method should accept two arguments, the array for the current
-  #line of people (katz_deli), and a string containing the name of the person joining the end of the
-  #line. The method should call out (puts) the person's name along with their position
-  #in line. Top-Tip: Remember that people like to count from 1, not from 0 ("zero") like computers.
+  #The take_a_number method should accept two arguments, the array for the current line of people (katz_deli), and a string containing the name of the person joining the end of the
+  #line. The method should call out (puts) the person's name along with their position in line. Top-Tip: Remember that people like to count from 1, not from 0 ("zero") like computers.
 #binding.pry
 def take_a_number(katz_deli, string)
   katz_deli << string
